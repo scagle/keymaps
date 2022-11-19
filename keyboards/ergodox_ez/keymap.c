2d27058529ddadd7b38c52b8a3bece8e6edc5f25
Steven Cagle <sjcagle1@gmail.com>
2022-11-18 23:04:58 -0800

    Updated CKC_DOCS in ergodox_ez

--- keyboards/ergodox_ez/keymap.c
+++ keyboards/ergodox_ez/keymap.c
@@ -298,7 +298,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
 
             if (record->event.pressed)
             {
-                SEND_STRING("https://tinyurl.com/583966pt");
+                SEND_STRING("https://tinyurl.com/22ukfj94");
             }
 
             return false;
